388e0f552d434d07766100c80921bd8cb4e1077b
Ghaida zalfa <ghaidagabang17@gmail.com>
2026-05-22 08:14:21 +0700

    asdas

--- config/cors.php
+++ config/cors.php
@@ -20,15 +20,14 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',      // Vite dev server (website)
-        'http://localhost:3000',      // Alternative dev server
-        'http://localhost:4033',      // Alternative dev server
         'https://ruangin-admin.soundofiwu.com',    // Production website domain
         'https://apiruangints3q67vtombd3ztzr98jkgsu.soundofiwu.com',    // API domain
     ],
 
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins_patterns' => [
+        '/^http:\/\/localhost:\d+$/',
+        '/^http:\/\/127\.0\.0\.1:\d+$/',
+    ],
     'allowed_headers' => ['*'],
 
     'exposed_headers' => ['authorization', 'x-auth-token'],
